e7d6d2a11f990f3bfa8e13d2c9469fbc3563b5b9
David <david-bourrel@hotmail.fr>
2021-06-05 17:01:47 +0200

    Résolution du test

--- capitalizeFirstLetters.js
+++ capitalizeFirstLetters.js
@@ -1,5 +1,14 @@
 const assert = require('assert');
 
+function capitalizeFirstLetters(input) {
+  return input.length > 0
+    ? input
+        .split(' ')
+        .map((e) => e[0].toUpperCase() + e.slice(1))
+        .join(' ')
+    : '';
+}
+
 assert.strictEqual(typeof capitalizeFirstLetters, 'function');
 assert.strictEqual(capitalizeFirstLetters.length, 1);
 assert.strictEqual(
